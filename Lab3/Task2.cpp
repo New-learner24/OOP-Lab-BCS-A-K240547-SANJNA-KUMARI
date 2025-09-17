55cc1150e205e03381442aac44b213e8fb08b242
New-learner24 <sanjanalund@gmail.com>
2025-09-17 19:55:55 +0500

    Update Task2.cpp

--- Lab3/Task2.cpp
+++ Lab3/Task2.cpp
@@ -1,1 +1,23 @@
-
+#include<iostream>
+#include<string>
+using namespace std;
+class Student{
+private:
+int rollNumber;
+public:
+string name;
+void setRollNo(int r){
+    rollNumber=r;
+}
+int getRollNo(){
+    return rollNumber;
+}
+};
+int main(){
+Student s1;
+int n;
+cout<<"Enter roll number: ";
+cin>>n;
+s1.setRollNo(n);
+cout<<"The roll no is = "<<s1.getRollNo()<<endl;
+}
