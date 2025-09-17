98e02af245d0f4be7ccaa4311591673c2b5b9cc4
New-learner24 <sanjanalund@gmail.com>
2025-09-17 19:54:43 +0500

    Update task1.cpp

--- Lab3/task1.cpp
+++ Lab3/task1.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+#include<string>
+using namespace std;
+class Student{
+public:
+string name;
+int rollNumber;
+};
+int main(){
+Student s1;
+cout<<"Enter name: ";
+getline(cin, s1.name);
+cout<<"Enter rollNumber: ";
+cin>>s1.rollNumber;
+cout<<"Name = "<<s1.name<<", RollNumber = "<<s1.rollNumber<<endl;
+}
